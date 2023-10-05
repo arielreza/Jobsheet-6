42db805d38dc317a3347b9d4315ff43b20537704
Arielreza <arielreza111@gmail.com>
2023-10-05 13:46:52 +0700

    FIX

--- Pemilihan2Percobaan118.java
+++ Pemilihan2Percobaan118.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
+
 public class Pemilihan2Percobaan118 {
 
     public static void main(String[] args) {
         Scanner input18 = new Scanner(System.in);
-        System.out.print("Masukkkan Tahun : ");
+        System.out.print("Masukkan Tahun: ");
         int tahun = input18.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
                 System.out.println("Tahun Kabisat");
-             } else {
-                System.out.println("Bukan Tahun Kabisat");
-             }
             } else {
                 System.out.println("Bukan Tahun Kabisat");
             }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
         }
     }
+}
